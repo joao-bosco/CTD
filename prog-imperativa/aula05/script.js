ff3a772854b1eecbca31da7d711d1f6e81e9071f
João Bosco S. Filho <67097255+joao-bosco@users.noreply.github.com>
2021-08-19 05:59:11 -0300

    Arquivos aula 05 e 06

---
+++ prog-imperativa/aula05/script.js
@@ -0,0 +1,15 @@
+
+//Criando a função
+function cumprimentar () {
+    //Inseri-se o código
+    alert('Olá, realizando testes')
+}
+// Criando função com parâmetro
+function saudacao (saudacao){
+    alert (saudacao);
+}
+
+//Invocação da função
+cumprimentar(); //Chamamos método
+saudacao ('Oi');
+saudacao ('Tudo bem com você');
